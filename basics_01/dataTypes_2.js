20568b8895555c520e57b7d8c43c9d55b8537ec3
RjDevV <ranjansingh9122004@gmail.com>
2024-07-30 08:06:50 +0000

    conversion operation

--- basics_01/dataTypes_2.js
+++ basics_01/dataTypes_2.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Ranjan")
 
 
-let name = "hitesh"
+let name = "Ranjan"
 let age = 18
 let isLoggedIn = false
 let state;
